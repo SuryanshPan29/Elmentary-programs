30b33a7e144daea354899ae5bf18e71365de4d79
SuryanshPan29 <51928098+SuryanshPan29@users.noreply.github.com>
2019-07-10 14:28:23 +0530

    Delete EL_26.c

--- EL_26.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float farh,calsius;
-    printf("Please enter the temp in calsius for which you want temp in the farh \n");
-    scanf("%f",&calsius);
-    farh=((9.0/5.0)*calsius)+32;
-    printf("%f",farh);
-}
