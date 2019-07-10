c45117c7f972bef9af94798332a65b3753c2393a
SuryanshPan29 <51928098+SuryanshPan29@users.noreply.github.com>
2019-07-10 14:25:02 +0530

    Delete EL_2.c

--- EL_2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{    char name[20];
-    printf("Please enter the name:-");
-    fgets(name,20,stdin);
-    printf("Welcome %s in this world of programing",name);
-}
-
